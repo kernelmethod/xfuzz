507b89a0fd4a63db9969d690ce44b4151cbe1d09
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2022-10-20 14:29:48 -0400

    Fixes to the run-tests.sh script.
    The run-tests.sh script now copies all files from the student's code
    directory into a temporary directory so that it can fix permissions on
    those files. This is useful in circumstances where the student may have
    set different permissions on their files (e.g. umask 0077) that would
    cause us to be unable to read the files during testing.
    
    In addition, the script now checks for the presence of an XFUZZ_COMMAND
    environment variable. If it finds that variable, it runs it in place of
    the default testing script.

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+DEFAULT_USE_APPARMOR=0
+DEFAULT_COMMAND="/usr/local/bin/run-tests.sh"
+
 main() {
     CODE_DIRECTORY=$(readlink -m "$1")
 
@@ -15,17 +18,39 @@ main() {
         COMPOSE_FILES="${COMPOSE_FILES} -f overrides/apparmor.yml"
     fi
 
+    # We temporarily copy the files in $CODE_DIRECTORY to a temporary directory
+    XFUZZ_TMP=$(mktemp -d)
+    echo "Temporarily copying files to $XFUZZ_TMP..."
+    cp -r --no-preserve=mode,ownership "${CODE_DIRECTORY}" "${XFUZZ_TMP}/xfuzz"
+    chmod -R go+rx "${XFUZZ_TMP}/xfuzz"
+    echo ""
+
     set -x
+    ls -la "${XFUZZ_TMP}/xfuzz"
     docker compose ${COMPOSE_FILES} \
         run --rm -i \
-        -v "${CODE_DIRECTORY}":/opt/xfuzz/xfuzz:ro \
-        xfuzz-client /usr/local/bin/run-tests.sh
+        -v "${XFUZZ_TMP}/xfuzz":/opt/xfuzz/xfuzz:ro \
+        xfuzz-client $(echo "${COMMAND}") \
+        || true
+    set +x
+
+    echo "Cleaning up ${XFUZZ_TMP}..."
+    rm -rf "${XFUZZ_TMP}"
 }
 
 usage() {
     cat << EOF >&2
 Usage: ./$(basename $0) /path/to/xfuzz/code
 
+Environment variables:
+
+    XFUZZ_USE_APPARMOR
+        Specifies whether or not the custom AppArmor profile specified in xfuzz.profile
+        should be used. This parameter should be set to 0 or 1 (defaults to ${DEFAULT_USE_APPARMOR}).
+
+    XFUZZ_COMMAND
+        The command to run in the Docker container run by the script. It may be useful
+        to override this for debugging purposes. Defaults to ${DEFAULT_COMMAND}
 EOF
 
     exit ${1:-1}
@@ -35,5 +60,6 @@ if [ ! $# -eq 1 ]; then
     usage
 fi
 
-USE_APPARMOR="${XFUZZ_USE_APPARMOR:-0}"
+USE_APPARMOR="${XFUZZ_USE_APPARMOR:-${DEFAULT_USE_APPARMOR}}"
+COMMAND="${XFUZZ_COMMAND:-${DEFAULT_COMMAND}}"
 main "$1"
